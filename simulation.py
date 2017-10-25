03d3f3d080c6ff1ee3a991a091186cbd189838b5
Mike Wittie <mwittie@cs.montana.edu>
2017-10-25 10:15:54 -0600

    Fall 2016 version of the assignment

---
+++ simulation.py
@@ -0,0 +1,65 @@
+'''
+Created on Oct 12, 2016
+
+@author: mwitt_000
+'''
+import network
+import link
+import threading
+from time import sleep
+
+##configuration parameters
+router_queue_size = 0 #0 means unlimited
+simulation_time = 1 #give the network sufficient time to transfer all packets before quitting
+
+if __name__ == '__main__':
+    object_L = [] #keeps track of objects, so we can kill their threads
+    
+    #create network nodes
+    client = network.Host(1)
+    object_L.append(client)
+    server = network.Host(2)
+    object_L.append(server)
+    router_a = network.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_a)
+    
+    #create a Link Layer to keep track of links between network nodes
+    link_layer = link.LinkLayer()
+    object_L.append(link_layer)
+    
+    #add all the links
+    link_layer.add_link(link.Link(client, 0, router_a, 0, 50))
+    link_layer.add_link(link.Link(router_a, 0, server, 0, 50))
+    
+    
+    #start all the objects
+    thread_L = []
+    thread_L.append(threading.Thread(name=client.__str__(), target=client.run))
+    thread_L.append(threading.Thread(name=server.__str__(), target=server.run))
+    thread_L.append(threading.Thread(name=router_a.__str__(), target=router_a.run))
+    
+    thread_L.append(threading.Thread(name="Network", target=link_layer.run))
+    
+    for t in thread_L:
+        t.start()
+    
+    
+    #create some send events    
+    for i in range(3):
+        client.udt_send(2, 'Sample data %d' % i)
+    
+    
+    #give the network sufficient time to transfer all packets before quitting
+    sleep(simulation_time)
+    
+    #join all threads
+    for o in object_L:
+        o.stop = True
+    for t in thread_L:
+        t.join()
+        
+    print("All simulation threads joined")
+
+
+
+# writes to host periodically
